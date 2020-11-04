4d445ebae8a61c8c17a9360d68f300fe90db64c3
Bryce Fujita <fujita63t@gmail.com>
2020-11-04 15:44:36 -0800

    To show change on local machine

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,3 +1,4 @@
+//Commit change./
 package Application;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -24,7 +25,7 @@ public class Main {
         }
         /* Turn off metal's use of bold fonts */
         UIManager.put("swing.boldMetal", Boolean.FALSE);
-
+        
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
